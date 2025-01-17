003b3cfe967523b7f9674a7690bbdb615d1f6490
chinawiz <chinawiz@gmail.com>
2025-01-17 21:16:53 +0800

    Update main.py
    添加 ngrok,使用公共url访问

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 import uvicorn
 from loguru import logger
-
+from pyngrok import ngrok
 from app.config import config
 
+# Ensure pyngrok is installed
+try:
+    import pyngrok
+except ImportError:
+    os.system("pip install pyngrok")
+
 if __name__ == "__main__":
+    # Set up ngrok
+    public_url = ngrok.connect(config.listen_port)
+    logger.info(f"ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{config.listen_port}\"")
+
     logger.info(
-        "start server, docs: http://127.0.0.1:" + str(config.listen_port) + "/docs"
+        "start server, docs: " + public_url + "/docs"
     )
     os.environ["HTTP_PROXY"] = config.proxy.get("http")
     os.environ["HTTPS_PROXY"] = config.proxy.get("https")
